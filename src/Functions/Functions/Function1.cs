9b63b293eb5c4f19a86276399d29916a4841b766
Gabriel-Cordeiro <gabrielsilvacordeiro@outlook.com>
2021-03-16 14:25:15 -0300

    adds serilog

--- src/Functions/Functions/Function1.cs
+++ src/Functions/Functions/Function1.cs
@@ -5,13 +5,20 @@ using Microsoft.Extensions.Logging;
 
 namespace Functions
 {
-    public static class Function1
+    public class Function1
     {
-        [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
         {
-            Console.WriteLine("Log from Console write line"); 
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            _logger = logger;
+        }
+
+        [FunctionName("Function1")]
+        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer)
+        {
+            Console.WriteLine("Log from Console write line");
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
 }
---
+++ src/Functions/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Functions;
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Serilog;
+using Serilog.Core;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var services = builder.Services;
+            var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            builder.Services.AddLogging(lb => lb.AddSerilog(ConfiguracaoLog(configuration)));
+        }
+
+        private Logger ConfiguracaoLog(IConfiguration configuration) => new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("ApplicationName", typeof(Startup).Assembly.GetName().Name)
+            .WriteTo.File(
+              $@"D:\home\LogFiles\Application\fnc-intreclame.txt",
+              fileSizeLimitBytes: 1_000_000,
+              rollOnFileSizeLimit: true,
+              shared: true,
+              flushToDiskInterval: TimeSpan.FromSeconds(1))
+            .CreateLogger();
+    }
+}
